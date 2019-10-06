6720c6a054740355df260985960334c288b93cbe
Marc Skov Madsen <MASMA@orsted.dk>
2019-10-06 11:10:05 +0200

    Added spreadsheet

--- src/app.py
+++ src/app.py
@@ -1,7 +1,38 @@
 import streamlit as st
-import pathlib
+import pandas as pd
 
-url = pathlib.Path(__file__).parent.parent / "AWESOMESTREAMLIT.md"
-with open(url, mode="r") as file:
-    readme_md_contents = "".join(file.readlines())
-st.markdown(readme_md_contents)
+"""# Streamlit Spreadsheet"""
+
+sheet = st.sidebar.selectbox("Select Sheet", ["Biostats", "Other"])
+
+st.write(f"## {sheet}")
+
+if sheet == "Biostats":
+    biostats_url = "https://people.sc.fsu.edu/~jburkardt/data/csv/biostats.csv"
+
+    biostats = pd.read_csv(biostats_url)
+
+    columns = {
+        "Name": "Name",
+        '     "Sex"': "Sex",
+        ' "Age"': "Age",
+        ' "Height (in)"': "Height (in)",
+        ' "Weight (lbs)"': "Weight (lbs)",
+    }
+    biostats = biostats.rename(columns=columns)
+
+    # category = st.selectbox("Category", ["Name", "Sex"])
+    measure = st.selectbox("Measure", ["Age", "Height (in)", "Weight (lbs)"])
+
+    # biostats_selected = biostats.set_index(category)
+    biostats_selected = pd.DataFrame(biostats[measure])
+    # biostats_selected.set_index(biostats[category], inplace=True)
+    st.bar_chart(biostats_selected)
+
+    f"""Source
+
+    {biostats_url}"""
+    biostats
+
+elif sheet == "Other":
+    pass
