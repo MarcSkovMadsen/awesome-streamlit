9240c4ee253ce972421da32bc24a17d690290563
Marc Skov Madsen <MASMA@orsted.dk>
2019-10-13 06:56:46 +0200

    issue 7 example added

---
+++ scratchpad/issue7/issue7_state_is_save.py
@@ -0,0 +1,13 @@
+"""This is a minimum replicating example of this issue
+https://github.com/MarcSkovMadsen/awesome-streamlit/issues/7"""
+import streamlit as st
+
+pages = ["Home", "Resources"]
+tags = ["Awesome", "Social"]
+resources = [{"name": ""}]
+
+page = st.sidebar.radio("Navigate", options=pages)
+st.title(page)
+if page == "Resources":
+    selection = st.multiselect("Select tag", tags)
+    st.write(selection)
