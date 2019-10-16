1273e59d4b6377719d314f8a5d493ae9cb0e3f4f
Marc Skov Madsen <MASMA@orsted.dk>
2019-10-16 05:25:17 +0200

    added tests of tags, authors and resources

---
+++ package/awesome_streamlit/database/__init__.py
@@ -0,0 +1,4 @@
+"""Imports to be exposed to the user of the package are listed here"""
+from awesome_streamlit.database.tags import TAGS
+from awesome_streamlit.database.authors import AUTHORS
+from awesome_streamlit.database.resources import RESOURCES
---
+++ package/tests/database/test_database.py
@@ -0,0 +1,21 @@
+"""Test of the database module"""
+from awesome_streamlit.database import TAGS, RESOURCES, AUTHORS
+
+
+def test_tags():
+    """Test TAGS"""
+    assert isinstance(TAGS, list)
+    assert len(TAGS) > 0
+
+
+def test_authors():
+    """Test AUTHORS"""
+    assert isinstance(AUTHORS, list)
+    assert len(AUTHORS) > 0
+
+
+def test_resources():
+    """Test RESOURCES"""
+    assert isinstance(RESOURCES, list)
+    assert len(RESOURCES) > 0
+
