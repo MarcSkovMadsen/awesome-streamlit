bf540ee37b7a4ba0413084b5a6405c656a1ca795
Marc Skov Madsen <MASMA@orsted.dk>
2019-10-20 07:07:08 +0200

    added

---
+++ scratchpad/issues_streamlit/issue_main_thread_is_not_in_main_loop.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import numpy as np
+from scipy.stats import norm
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def write():
+    plot_section()
+
+
+def plot_section():
+    st.markdown(
+        """
+## Interactive plot - Streamlit
+
+"""
+    )
+    x = get_x()
+    mu = st.slider(
+        "mu", value=float(0), min_value=float(-5), max_value=float(5), step=float(0.1)
+    )
+    sigma = st.slider(
+        "sigma",
+        value=float(1),
+        min_value=float(0.1),
+        max_value=float(5),
+        step=float(0.1),
+    )
+    plot_figure(x, mu, sigma)
+
+
+@st.cache
+def get_x():
+    return np.linspace(-10, 10, 200)
+
+
+def plot_figure(x, mu: float = 0, sigma: float = 1):
+    y = norm.pdf(x, mu, sigma)
+    title = f"Gaussian Density (mu = {mu} and sigma = {sigma})"
+    plt.hist(y, bins=20)
+    st.pyplot()
+
+
+write()
---
+++ scratchpad/issues_streamlit/issue_slider_does_not_take_combination_of_int_and_float.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.slider("mu", value=0, min_value=-5, max_value=5, step=0.1)
+
