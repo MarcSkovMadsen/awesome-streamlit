79316cfa14bb24ee69ed15cedd685736a4165074
Marc Skov Madsen <MASMA@orsted.dk>
2019-10-22 03:15:23 +0200

    created

---
+++ scratchpad/issues_streamlit/issue_layout_takes_a_lot_of_space.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+data = {"x": [1, 2, 3, 4], "y": [2, 4, 6, 8]}
+
+st.selectbox("Select x", options=["A", "B"])
+st.selectbox("Select y", options=["A", "B"])
+dataframe = pd.DataFrame(data)
+fig = px.scatter(dataframe, x="x", y="x", title="Plot 1", height=400)
+st.plotly_chart(fig, height=400)
+st.slider("Select min and max", min_value=1, max_value=10, value=[1, 4])
---
+++ scratchpad/issues_streamlit/issue_plot_and_slider_overlap.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+data = {"x": [1, 2, 3, 4], "y": [2, 4, 6, 8]}
+
+dataframe = pd.DataFrame(data)
+fig = px.scatter(dataframe, x="x", y="x", title="Plot 1")
+st.plotly_chart(fig)
+st.slider("Select min and max", min_value=1, max_value=10, value=[1, 4])
+
+fig = px.scatter(dataframe, x="x", y="x", title="Plot 2")
+st.plotly_chart(fig, height=400)
+st.slider("Select min and max 2", min_value=1, max_value=10, value=[1, 4])
+
+fig = px.scatter(dataframe, x="x", y="x", height=400, title="Plot 3")
+st.plotly_chart(fig)
+st.slider("Select min and max 2", min_value=1, max_value=10, value=[1, 4])
