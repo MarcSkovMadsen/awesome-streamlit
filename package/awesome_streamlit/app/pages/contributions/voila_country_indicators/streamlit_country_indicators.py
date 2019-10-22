66323324a0e5b2df4f5c88fc73e3d5b6676befd6
Marc Skov Madsen <MASMA@orsted.dk>
2019-10-22 03:18:00 +0200

    clean up

--- package/awesome_streamlit/app/pages/contributions/voila_country_indicators/streamlit_country_indicators.py
+++ package/awesome_streamlit/app/pages/contributions/voila_country_indicators/streamlit_country_indicators.py
@@ -11,62 +11,44 @@ The benchmark example from Voila is
 Author: Marc Skov Madsen https://github.com/marcskovmadsen
 
 """
+from typing import Tuple
+
 import pandas as pd
-import streamlit as st
 import plotly.express as px
+import streamlit as st
 
+DATA_PATH = (
+    "package/awesome_streamlit/app/pages/contributions/voila_country_indicators/"
+    "country_indicators.csv"
+)
+DATA_URL = (
+    "https://gist.githubusercontent.com/chriddyp/cb5392c35661370d95f300086accea51/"
+    "raw/8e0768211f6b747c0db42a9ce9a0937dafcbd8b2/indicators.csv"
+)
 
-DATA_PATH = "package/awesome_streamlit/app/pages/contributions/voila_country_indicators/country_indicators.csv"
-DATA_URL = "https://gist.githubusercontent.com/chriddyp/cb5392c35661370d95f300086accea51/raw/8e0768211f6b747c0db42a9ce9a0937dafcbd8b2/indicators.csv"
+EXPLANATION = """
+## Compare different development indicators.
 
-EXPLANATION = """\
-<div class="app-sidebar">
-<p><em>Compare different development indicators.</em><p>
+Select what indicators to plot in the dropdowns, and use the slider
+to sub-select a fraction of years to include in the plot.
 
-<p>Select what indicators to plot in the dropdowns, and use the slider
-to sub-select a fraction of years to include in the plot.</p>
+Data and idea copied from the
+[Plotly Dash documentation](https://dash.plot.ly/getting-started-part-2)
 
-<p>Data and idea copied from the <a href="https://dash.plot.ly/getting-started-part-2">
-Plotly Dash documentation</a>.</p>
-
-<p>This example demonstrates combining Plotly with Streamlit widgets.
-</div>
+This example demonstrates combining Plotly with Streamlit widgets.
 """
 
 
-def write():
+def main():
+    """This is the main component. Run this to start the app."""
     intro_section()
     streamlit_section()
     voila_section()
     findings_section()
 
 
-def streamlit_section():
-    st.markdown(
-        """
-## Plot - Streamlit
-
-"""
-    )
-    App.from_url(DATA_PATH)
-
-
-def voila_section():
-    st.markdown(
-        """
-## Plot - Voila
-
-![Executing 3 of 6 spinner](voila_executing_3_of_6)
-
-We compare to the Country Indicator app in the Voila Gallery.
-Click [here](https://github.com/voila-gallery/voila-gallery-country-indicators) to see the source.
-
-![Voila country indicators](voila_country_indicators.png)
-"""
-    )
-
-
 def intro_section():
+    """This section introduces the app"""
     st.markdown(
         """
 # Country Indicators
@@ -84,6 +66,35 @@ As of today (2019-10-20) both Voila and Streamlit are released for Beta Testing 
     )
 
 
+def streamlit_section():
+    """This section controls the Streamlit Country Indicators App"""
+    st.markdown(
+        """
+## Plot - Streamlit
+
+"""
+    )
+    App.from_url(DATA_PATH)
+
+
+def voila_section():
+    """This section controls the Voila Reference Country Indicators App"""
+    st.markdown(
+        """
+## Plot - Voila
+
+We compare to the Country Indicator app in the Voila Gallery.
+**Click the image**  to see the **source**.
+
+[![Voila country indicators](https://github.com/MarcSkovMadsen/awesome-streamlit/blob/master/package/awesome_streamlit/app/pages/contributions/voila_country_indicators/voila_country_indicators.png?raw=true)](https://github.com/voila-gallery/voila-gallery-country-indicators)
+
+Voila also shows a nice spinner by it self while running some of the cells
+
+![Executing 3 of 6 spinner](https://github.com/MarcSkovMadsen/awesome-streamlit/blob/master/package/awesome_streamlit/app/pages/contributions/voila_country_indicators/voila_executing_3_of_6.png?raw=true)
+"""
+    )
+
+
 def findings_section():
     st.markdown(
         """
@@ -92,7 +103,7 @@ def findings_section():
 ### Pros of Voila
 
 - The label and widget are independent. I.e. you don't have to have a label.
-- There is a built in IntRangeSelector widget.
+- You don't need to know a trick to get a IntRangeSlider.
 
 ### Pros of Streamlit
 
@@ -109,59 +120,66 @@ because of very fast, automatic hot reload and ease of navigation in editor.
 
 - Streamlit:
   - [@st.cache @classmethod issue 481](https://github.com/streamlit/streamlit/issues/481)
+  - [slider range method is not 'obvious' issue 482](https://github.com/streamlit/streamlit/issues/482)
+  - [Plotly_chart and slider overlap issue 379](https://github.com/streamlit/streamlit/issues/379)
+  - [Compact and Dashboard layout needed issue 486](https://github.com/streamlit/streamlit/issues/486)
   - [No Grid issues](https://github.com/streamlit/streamlit/issues?utf8=%E2%9C%93&q=is%3Aissue+is%3Aopen+grid)
-  - Plotly_chart and slider overlap
 """
     )
 
 
 class App:
+    """The Voila reference example is implemented as an App. Thus we do the same for Streamlit"""
+
     def __init__(self, df):
-        self._df = df.copy(deep=True)
-        st.sidebar.markdown(EXPLANATION, unsafe_allow_html=True)
+        self._df = df.copy(deep=True)  # Deep copy to avoid st.cache problems
         available_indicators = self._df["Indicator Name"].unique()
+        min_value = min(df["Year"])
+        max_value = max(df["Year"])
+
+        st.sidebar.markdown(EXPLANATION, unsafe_allow_html=True)
         x_indicator = st.selectbox("Select indicator x", available_indicators, 0)
         y_indicator = st.selectbox("Select indicator y", available_indicators, 1)
         plotly_chart = st.empty()
+
+        # Hack to seperate plot and slider
         st.markdown("<br><br>", unsafe_allow_html=True)
-        min_value = min(df["Year"])
-        max_value = max(df["Year"])
         year_range = st.slider(
             "Select min and max Year",
             min_value=min_value,
             max_value=max_value,
             value=[min_value, max_value],
         )
+
         fig = self._create_plot(x_indicator, y_indicator, year_range)
-        plotly_chart.plotly_chart(fig)
+        plotly_chart.plotly_chart(fig, width=0, height=300)
 
     @classmethod
     @st.cache
     def _get_dataframe(cls, url):
+        """Return the dataframe of country indicators"""
         return pd.read_csv(url)
 
     @classmethod
     def from_url(cls, url) -> "App":
+        """Creates an instance of the App with data from the url"""
         df = cls._get_dataframe(url)
         return cls(df)
 
-    def _create_plot(self, x_indicator, y_indicator, year_range):
+    def _create_plot(
+        self, x_indicator: str, y_indicator: str, year_range: Tuple[int, int]
+    ) -> "plotly.Fig":
+        """Creates a Plotly plot for the specified selections"""
         df = self._df[self._df["Year"].between(*year_range)]
         xs = df[df["Indicator Name"] == x_indicator]
         ys = df[df["Indicator Name"] == y_indicator]
 
         dataframe = pd.merge(xs, ys, how="inner", on=["Country Name", "Year"])
         title = f"Country Indicators"
-        fig = px.scatter(dataframe, x="Value_x", y="Value_y", title=title)
+        fig = px.scatter(dataframe, x="Value_x", y="Value_y", title=title, height=400)
         fig.update_layout(dict(xaxis=dict(title=dict(text=x_indicator))))
         fig.update_layout(dict(yaxis=dict(title=dict(text=y_indicator))))
         return fig
 
 
-def range_selectbox(label, min_value: int, max_value: int):
-    selection_min = st.slider(f"Select min {label}", min_value, max_value)
-    selection_max = st.slider(f"Select max year {label}", min_value, max_value)
-    return range(selection_min, selection_max)
-
-
-write()
+main()
