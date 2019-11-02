22133d03bff917488f2a618ead0f16cf6492ecf7
Marc Skov Madsen <MASMA@orsted.dk>
2019-11-02 21:39:43 +0100

    removed local file load

--- gallery/ml_app_registry/app.py
+++ gallery/ml_app_registry/app.py
@@ -139,15 +139,9 @@ def get_local_path(file: str) -> pathlib.Path:
 @st.cache
 def get_pickle(file: str):
     """An instance of an object from the pickle file"""
-    try:
-        local_file = get_local_path(file)
-        if local_file.exists() and False:
-            with open(local_file, "rb") as open_file:
-                return pickle.load(open_file)
-    except:  # pylint: disable=bare-except
-        github_url = GITHUB_ROOT + file
-        with urllib.request.urlopen(github_url) as open_file:  # type: ignore
-            return pickle.load(open_file)
+    github_url = GITHUB_ROOT + file
+    with urllib.request.urlopen(github_url) as open_file:  # type: ignore
+        return pickle.load(open_file)
 
 
 main()
