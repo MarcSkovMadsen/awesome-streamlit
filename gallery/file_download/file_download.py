304aab4e91186b1ecdf456d43c9b44ecee43c10b
Marc Skov Madsen <MASMA@orsted.dk>
2019-12-04 11:54:59 +0100

    added more explaining text

--- gallery/file_download/file_download.py
+++ gallery/file_download/file_download.py
@@ -10,7 +10,13 @@ text = """\
     But I discovered a workaround
     [here](https://github.com/holoviz/panel/issues/839#issuecomment-561538340).
 
-    You can try it out below.
+    It's based on the concept of
+    [HTML Data URLs](https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Data_URIs)
+
+    You can try it out below for a dataframe csv file download.
+
+    The methodology can be extended to other file types. For inspiration see
+    [base64.guru](https://base64.guru/converter/encode/file)
     """
 st.markdown(text)
 
@@ -19,5 +25,5 @@ data = [(1, 2, 3)]
 df = pd.DataFrame(data, columns=["Col1", "Col2", "Col3"])
 csv = df.to_csv(index=False)
 b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-href = f'<a href="data:file/csv;base64,{b64}">Download Table</a> (right-click and save as &lt;some_name&gt;.csv)'
+href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
 st.markdown(href, unsafe_allow_html=True)
