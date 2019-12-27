67e450552884abfc0cd02adadc534a0d16180d06
Marc Skov Madsen <MASMA@orsted.dk>
2019-12-27 08:39:14 +0100

    small refactor

--- gallery/file_uploader_multiple_files/file_uploader_multiple_files.py
+++ gallery/file_uploader_multiple_files/file_uploader_multiple_files.py
@@ -1,17 +1,20 @@
 """This is a example shows how to upload multiple files via the File Uploader Widget
 
 Once you upload multiple files it triggers multiple reruns of your script. One for each file.
-You then need to process those files and maybe use store them in a List or Dictionary.
+You then need to process those files and maybe store them in a static List or Dictionary.
 Here I store them in a List.
 
 This example was based on
 [Discourse 1445](https://discuss.streamlit.io/t/uploading-multiple-files-with-file-uploader/1445)
 """
+from typing import List
+
 import streamlit as st
 
 
 @st.cache(allow_output_mutation=True)
 def get_static_store():
+    """This List is initialized once and can be used to store the files uploaded"""
     return []
 
 
