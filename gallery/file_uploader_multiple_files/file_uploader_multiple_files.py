729bf6f07027e866bb7d32294784daf93b12b346
Marc Skov Madsen <MASMA@orsted.dk>
2019-12-27 09:50:58 +0100

    try to fix problems

--- gallery/file_uploader_multiple_files/file_uploader_multiple_files.py
+++ gallery/file_uploader_multiple_files/file_uploader_multiple_files.py
@@ -2,13 +2,18 @@
 """This is example shows how to **upload multiple files** via the
 [File Uploader Widget](https://streamlit.io/docs/api.html?highlight=file%20upload#streamlit.file_uploader)
 
-Once you upload multiple files it triggers multiple reruns of your script. One for each file.
-You then need to process those files and maybe store them in a static List or Dictionary.
-Here I store them in a Dictionary.
+As far as I can see you can only upload one file at a time. So if you need multiple files in your
+app, you need to store them in a static List or Dictionary. Alternatively they should be uploaded
+as one .zip file.
 
-Please note that file uploader is a **bit problematic** because every time you interact with any
-widget, the script is rerun and you risk processing or storing the file again! I've tried to handle
-that in the code below.
+Please note that file uploader is a **bit problematic** because
+- You can only upload one file at the time.
+- You get no additional information on the file like name, size, upload time, type etc. So you
+cannot distinguish the files without reading the content.
+- every time you interact with any widget, the script is rerun and you risk processing or storing
+the file again!
+- The file uploader widget is not cleared when you clear the cache and there is no way to clear the
+file uploader widget programmatically.
 
 This example was based on
 [Discourse 1445](https://discuss.streamlit.io/t/uploading-multiple-files-with-file-uploader/1445)
@@ -39,11 +44,12 @@ def main():
         if not value in static_store.values():
             static_store[result] = value
     else:
+        static_store.clear()  # Hack to clear list of the user clears the cache and reloads the page
         st.info("Upload one or more `.py` files.")
 
     if st.button("Clear file list"):
         static_store.clear()
-    if st.checkbox("Show file list?"):
+    if st.checkbox("Show file list?", True):
         st.write(list(static_store.keys()))
     if st.checkbox("Show content of files?"):
         for value in static_store.values():
