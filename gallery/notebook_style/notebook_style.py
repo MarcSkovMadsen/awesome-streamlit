3de180a0e4591b13fe96f6ee1399edee230131d5
Marc Skov Madsen <MASMA@orsted.dk>
2020-01-03 19:25:30 +0100

    added breakpoint

--- gallery/notebook_style/notebook_style.py
+++ gallery/notebook_style/notebook_style.py
@@ -31,6 +31,7 @@ def end_code_block(display=True):
     # stack[-2] is the frame in the user's "notebook" calling us
     # (as long as the user is calling us directly)
     filename, lineno = frame.filename, frame.lineno
+    breakpoint()
     with open_python_file(filename) as source_file:
         source_lines = source_file.readlines()
     # `lineno-1` means we skip showing the call to us
