be454e29c35a9a1a760b1737a5176a47f4f9717b
Marc Skov Madsen <MASMA@orsted.dk>
2020-07-28 06:08:51 +0200

    fix bug with missing folder from package

--- package/setup.py
+++ package/setup.py
@@ -6,7 +6,7 @@ or similar where . is replaced by the path to package root
 """
 import pathlib
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 README_FILE_PATH = pathlib.Path(__file__).parent / "README.md"
 with open(README_FILE_PATH) as f:
@@ -14,7 +14,7 @@ with open(README_FILE_PATH) as f:
 
 s = setup(  # pylint: disable=invalid-name
     name="awesome-streamlit",
-    version="20191018.1",
+    version="20200728.1",
     license="MIT",
     description="""This package supports the Awesome Streamlit Project and
     provides highly experimental features!""",
@@ -23,7 +23,7 @@ s = setup(  # pylint: disable=invalid-name
     url="https://github.com/marcskovmadsen/awesome-streamlit",
     author="Marc Skov Madsen",
     author_email="marc.skov.madsen@gmail.com",
-    packages=["awesome_streamlit"],
+    packages=find_packages(include=["awesome_streamlit", "awesome_streamlit.*"]),
     install_requires=["streamlit>=0.47.4"],
     python_requires=">= 3.7",
     zip_safe=False,
